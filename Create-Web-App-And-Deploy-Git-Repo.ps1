27a19b140d983883d5dbca3acd4c79a65e16e83d
Joshua Rudio (InConsulting Inc.) <v-jorudi@microsoft.com>
2018-03-09 14:41:29 -0800

    Add script to create web app and deploy git repo

---
+++ Create-Web-App-And-Deploy-Git-Repo.ps1
@@ -0,0 +1,46 @@
+﻿$gitRepo = "<Repo url goes here>"
+$webAppName = "<name your web app>"
+$location = "westus2"
+$resourceGroup = "<rg name goes here>"
+
+# Login
+Login-AzureRmAccount
+
+# Make a new resource group.
+New-AzureRmResourceGroup -Name $resourceGroup -Location $location
+
+# Create the 'Free' tier App Service plan.
+New-AzureRmAppServicePlan -Name $webAppName `
+                          -Location $location `
+                          -ResourceGroupName $resourceGroup `
+                          -Tier Free
+
+# Create the web app.
+New-AzureRmWebApp -Name $webAppName `
+                  -Location $location `
+                  -AppServicePlan $webAppName `
+                  -ResourceGroupName $resourceGroup
+
+# Configure GitHub deployment from your GitHub repo and deploy once.
+$PropertiesObject = @{
+    scmType="LocalGit";
+}
+Set-AzureRmResource -PropertyObject $PropertiesObject `
+                    -ResourceGroupName $resourceGroup `
+                    -ResourceType Microsoft.Web/sites/config `
+                    -ResourceName $webAppName/web `
+                    -ApiVersion 2015-08-01 `
+                    -Force
+
+# Get app-level deployment credentials
+$xml = [xml](Get-AzureRmWebAppPublishingProfile -Name $webAppName `
+                                                -ResourceGroupName $resourceGroup `
+                                                -OutputFile null
+)
+$username = $xml.SelectNodes("//publishProfile[@publishMethod=`"MSDeploy`"]/@userName").value
+$password = $xml.SelectNodes("//publishProfile[@publishMethod=`"MSDeploy`"]/@userPWD").value
+
+# Add the Azure remote to you local Git repo and push your code
+#### This method saves your password in the git remote. You can use Git credential manager to secure your password instead.
+git remote add azure "https://${username}:$password@$webappName.scm.azurewebsites.net"
+git push azure master
